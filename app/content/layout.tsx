d78803cb989554c48df86dd814afe96916c5c79f
aizak2118 <okazuki58@gmail.com>
2024-09-17 07:06:36 +0900

    first commit

---
+++ app/content/layout.tsx
@@ -0,0 +1,13 @@
+import Header from "@/app/ui/header";
+import { ReactNode } from "react";
+
+export default function MainLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main className="container mx-auto flex flex-col flex-1 justify-center items-center py-5">
+        {children}
+      </main>
+    </>
+  );
+}
