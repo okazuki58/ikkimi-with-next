4d6df002221ae349c53dbe5bbbc61f55a73bf690
aizak2118 <okazuki58@gmail.com>
2024-10-09 15:31:37 +0900

    スタイル(SearchModal.tsx): モーダルのスタイルを改善し、オーバーフローを追加 修正(SearchModal.tsx): モーダルのクラス名を更新して視覚的な一貫性を向上

--- app/components/modal/SearchModal.tsx
+++ app/components/modal/SearchModal.tsx
@@ -14,11 +14,11 @@ export default function SearchModal({ isOpen, onClose }: ModalComponentProps) {
         className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
       />
 
-      <div className="fixed inset-0 w-screen z-10">
+      <div className="fixed inset-0 w-screen z-10 overflow-y-auto">
         <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
           <DialogPanel
             transition
-            className="relative transform rounded-lg text-left shadow-xl transition-all  data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 w-full sm:max-w-lg "
+            className="relative transform overflow-hidden rounded-xl bg-white p-6 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:max-w-[25rem] w-full data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
           >
             <AlgoSearch onCloseModal={onClose} isOpen={isOpen} />
           </DialogPanel>
