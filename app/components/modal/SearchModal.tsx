4aa73eabd2acf7743a9378554ed54f6f99faf067
aizak2118 <okazuki58@gmail.com>
2024-10-09 15:33:36 +0900

    修正(SearchModal.tsx): AlgoSearchコンポーネントをコメントアウトし確認用のdivを追加 この変更は、デバッグや確認のために一時的にAlgoSearchを無効化するために行った。

--- app/components/modal/SearchModal.tsx
+++ app/components/modal/SearchModal.tsx
@@ -20,7 +20,8 @@ export default function SearchModal({ isOpen, onClose }: ModalComponentProps) {
             transition
             className="relative transform overflow-hidden rounded-xl bg-white p-6 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:max-w-[25rem] w-full data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
           >
-            <AlgoSearch onCloseModal={onClose} isOpen={isOpen} />
+            {/* <AlgoSearch onCloseModal={onClose} isOpen={isOpen} /> */}
+            <div>確認用</div>
           </DialogPanel>
         </div>
       </div>
